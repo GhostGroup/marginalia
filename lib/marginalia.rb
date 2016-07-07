270c485c77fb224e9bb147159b24c39b4bd999e7
Justin Craig-Kuhn <jcraigk@gmail.com>
2016-07-07 09:23:07 -0700

    Add sql comment string sequence to prevent sql errors

--- lib/marginalia.rb
+++ lib/marginalia.rb
@@ -42,7 +42,7 @@ module Marginalia
     def annotate_sql(sql)
       comment = Marginalia::Comment.construct_comment
       if comment.present? && !sql.include?(comment)
-        "#{sql}__;__#{comment}"
+        "#{sql} /*#{comment}*/"
       else
         sql
       end
